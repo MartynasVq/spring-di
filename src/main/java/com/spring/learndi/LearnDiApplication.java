be1381860517fed58f1cba709c5c4bd075ada361
Martynas <nbtsst@gmail.com>
2020-11-26 22:15:46 +0200

    initial commit

---
+++ src/main/java/com/spring/learndi/LearnDiApplication.java
@@ -0,0 +1,13 @@
+package com.spring.learndi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/learndi/LearnDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.learndi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
